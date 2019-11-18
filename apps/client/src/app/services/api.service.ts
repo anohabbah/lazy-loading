49b9d6faa2c84b1e50b97dbb7c469940db232441
Abbah <anohabbah@gmail.com>
2019-11-18 11:25:27 +0100

    use http param class to format http query

--- apps/client/src/app/services/api.service.ts
+++ apps/client/src/app/services/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Item } from '../entities/item.entity';
 import { map } from 'rxjs/operators';
@@ -15,7 +15,7 @@ export class ApiService {
 
   fetchItems(): Observable<Item[]> {
     return this.http
-      .get(`${this.baseURL}?pageNumber=${this.pageNumber}&pageSize=20` )
+      .get(this.baseURL, { params: new HttpParams().set('pageNumber', `${this.pageNumber}`).set('pageSize', `20`) })
       .pipe(map((res: { items }) => res.items));
   }
 
